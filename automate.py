9295ba81c6d63d986801608fca3c5892a95f3c5b
blaze7frost <72850454+blaze7frost@users.noreply.github.com>
2020-12-13 00:23:35 +0530

    Update automate.py

--- automate.py
+++ automate.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import datetime
 import time
 
 
@@ -18,8 +19,13 @@ login = driver.find_element_by_xpath('//*[@id="loginbtn"]')
 login.click()
 
 try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="join_button_input"]'))
-    )
-finally:
+    for i in range(5):
+        time.sleep(2)
+        driver.refresh()
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="join_button_input"]'))
+        )
+        time.sleep(2)
+
+except:    
     driver.quit()
